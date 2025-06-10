e5684f6df1c3b077c08eda344e208c31344ea6ab
Fred452 <fredyarce452@gmail.com>
2025-06-10 20:59:02 -0300

    Agregando redirección y notificación al crear un usuario en el recurso de usuario

--- app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,33 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    /**
+     * Redirije al indice después de crear un usuario.
+     *
+     * @return string
+     */
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    /**
+     * Obtiene la notificación que se mostrará después de crear un usuario.
+     *
+     * @return Notification|null
+     */
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Usuario creado')
+            ->body('El usuario ha sido creado exitosamente.');
+    }
+
 }
